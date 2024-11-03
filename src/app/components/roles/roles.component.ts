565d2c931b9e19ceb1d0c6c4bd8202ec832f765a
Gokul Kannan <gokulrgk1110@gmail.com>
2024-11-03 20:31:04 +0530

    Added the components and bindings

---
+++ src/app/components/roles/roles.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-roles',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './roles.component.html',
+  styleUrl: './roles.component.css'
+})
+export class RolesComponent {
+  firstName: string = "Angular Application";
+  angularVersion = "Version 18";
+
+  version: number = 18;
+  isActive: boolean = true;
+  currentDate: Date = new Date();
+  inputType: string = "radio";
+  selectedState: string = "";
+}
