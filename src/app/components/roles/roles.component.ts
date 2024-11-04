833106f802bfe8cfff8db33ede38e254c38fbaab
Gokul Kannan <gokulrgk1110@gmail.com>
2024-11-04 22:10:13 +0530

    API call to get data

--- src/app/components/roles/roles.component.ts
+++ src/app/components/roles/roles.component.ts
@@ -1,14 +1,36 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { IRole } from '../../model/interface/role';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-roles',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.css'
 })
-export class RolesComponent {
+export class RolesComponent implements OnInit {
+  
+  roleList: IRole[] =[];
+  http = inject(HttpClient);
+
+  // constructor(private http: HttpClient){
+
+  // }
+
+  ngOnInit(): void {
+    this.getAllRoles();
+  }
+
+  getAllRoles()
+  {
+    this.http.get("https://freeapi.miniprojectideas.com/api/ClientStrive/GetAllRoles").subscribe((res: any) =>{
+      this.roleList = res.data;
+    })
+  }
+
   firstName: string = "Angular Application";
   angularVersion = "Version 18";
 
