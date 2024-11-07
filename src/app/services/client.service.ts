5557f442485bbcba3902b8137d3479de861ed64e
Gokul Kannan <gokulrgk1110@gmail.com>
2024-11-07 22:09:06 +0530

    Added Environments files and API calls

--- src/app/services/client.service.ts
+++ src/app/services/client.service.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { APIResponseModel } from '../model/interface/role';
+import { environment } from '../../environments/environment';
+import { Client } from '../model/class/Client';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +14,16 @@ export class ClientService {
 
   GetAllClients():Observable<APIResponseModel>
   {
-    return this.http.get<APIResponseModel>("https://freeapi.miniprojectideas.com/api/ClientStrive/GetAllClients");
+    return this.http.get<APIResponseModel>(environment.API_URL + "GetAllClients");
+  }
+
+  AddUpdate(obj:Client):Observable<APIResponseModel>
+  {
+    return this.http.post<APIResponseModel>(environment.API_URL + "AddUpdateClient", obj);
+  }
+
+  DeleteClientById(id:number):Observable<APIResponseModel>
+  {
+    return this.http.delete<APIResponseModel>(environment.API_URL + "DeleteClientByClientId?clientId="+id );
   }
 }
--- src/app/services/master.service.ts
+++ src/app/services/master.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { APIResponseModel } from '../model/interface/role';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,6 @@ export class MasterService {
   constructor(private http: HttpClient) { }
 
   getAllDesignation(): Observable<APIResponseModel>{
-    return this.http.get<APIResponseModel>("https://freeapi.miniprojectideas.com/api/ClientStrive/GetAllDesignation");
+    return this.http.get<APIResponseModel>(environment.API_URL + "GetAllDesignation");
   }
 }
